1865f3380f7a110b24b4b6dd7f1366e605d5e683
sidx255 <siddharth190820@gmail.com>
2023-02-03 11:12:31 +0530

    chore: initialize with required files

--- src/server.js
+++ src/server.js
@@ -1,2 +1,14 @@
 
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json());
+
+// add route here
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
